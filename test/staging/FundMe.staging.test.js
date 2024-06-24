149d32dc5e2baed1ed89ea729801b4aa5b4aea93
psl012 <paulsamuel77@gmail.com>
2024-06-24 12:13:34 +0800

    fixed some stuffs

--- test/staging/FundMe.staging.test.js
+++ test/staging/FundMe.staging.test.js
@@ -15,8 +15,11 @@ developmentChains.includes(network.name) ?
         })
 
         it("allows people to fund and withdraw", async function(){
-            await fundMe.fund({value: sendValue})
-            await fundMe.withdraw()
+            const fundTxResponse = await fundMe.fund({ value: sendValue })
+            await fundTxResponse.wait(1)
+            
+            const withdrawTxResponse = await fundMe.withdraw()
+            await withdrawTxResponse.wait(1)
 
             const endingBalance = await ethers.provider.getBalance(
                 fundMe.target
